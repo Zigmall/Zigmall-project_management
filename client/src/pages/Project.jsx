81a495f8e851a01fef3c41465be11463e8176314
Rafal Hes <rafal.hes@yahoo.com>
2023-02-25 09:43:08 +0100

    Add ClientInfo component.

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -17,8 +17,8 @@ const Project = () => {
   return (
     <>
       {!loading && !error && (
-        <div className="mx-auto w-75 card p-5 ">
-          <Link to="/" className="btn btn-light btn-sm w-25 d-inline sm-auto">
+        <div className="mx-auto w-75 card p-5 mt-5">
+          <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
             Back
           </Link>
           <h1>{data.project.name}</h1>
