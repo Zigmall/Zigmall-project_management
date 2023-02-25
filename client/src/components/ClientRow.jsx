d7270c00650365117cd4dd67389a27af65192895
Rafal Hes <rafal.hes@yahoo.com>
2023-02-25 15:39:30 +0100

    Add warning when delete client.

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,6 +2,7 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientsQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 const ClientRow = ({ client }) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
@@ -9,14 +10,19 @@ const ClientRow = ({ client }) => {
       id: client.id
     },
     onCompleted: () => {
-        console.log('Client Deleted');
-        }
-        ,
-    onError: (error) => {
-        console.log(error);
+      console.log('Client Deleted');
     },
-    refetchQueries: [{ query: GET_CLIENTS }]
+    onError: (error) => {
+      console.log(error);
+    },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }]
   });
+
+  const handleDelete = () => {
+    if (window.confirm('Deleting a client will delete all projects associated with this client. Are you sure you want to delete this client?')) {
+      deleteClient();
+    }
+  };
 
   return (
     <tr>
@@ -24,7 +30,7 @@ const ClientRow = ({ client }) => {
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger d-block btn-sm" onClick={deleteClient}>
+        <button className="btn btn-danger d-block btn-sm" onClick={handleDelete}>
           <FaTrash />
         </button>
       </td>
